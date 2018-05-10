c2bc5726db49f91da8b027b9e738a7e50cbb4052
ZUNJAE <sunjayskhr@gmail.com>
2018-05-10 02:55:40 +0200

    Update WaifuRepository.py

--- source code/WaifuRepository.py
+++ source code/WaifuRepository.py
@@ -1,6 +1,6 @@
 import time
 
-import Zdb
+from KanonApp import Zdb
 
 
 def get_waifus_by_token(user_token):
@@ -19,6 +19,25 @@ def save_waifu(user_token, json_body):
                )
 
 
+def get_number_of_waifus(user_token):
+    return Zdb.get_json_result("SELECT COUNT(*) AS UserWaifuCount "
+                               "From UserWaifus "
+                               "WHERE UserId = ?", (user_token,))
+
+
+def get_top_waifus(number_of_waifus):
+    max_number_of_waifus = 15
+    if number_of_waifus > max_number_of_waifus:
+        number_of_waifus = max_number_of_waifus
+
+    return Zdb.get_json_result(
+        "SELECT count(WaifuId) as TotalFavoriteCount, WaifuName, AnimeId, WaifuName, ImageURL, DateCreated "
+        "FROM UserWaifus "
+        "GROUP BY WaifuId "
+        "ORDER BY TotalFavoriteCount DESC "
+        "LIMIT ?", (number_of_waifus,))
+
+
 def get_waifus_by_token_and_anime_id(user_token, anime_id):
     return Zdb.get_json_result(
         "SELECT WaifuId, AnimeId, WaifuName, ImageURL, DateCreated "
@@ -26,6 +45,12 @@ def get_waifus_by_token_and_anime_id(user_token, anime_id):
         "WHERE UserId = ? AND AnimeId = ? "
         "ORDER BY DateCreated DESC",
         (user_token, anime_id))
+
+
+def get_waifus_by_list_of_waifu_ids(user_token, list_of_waifu_ids):
+    query = "SELECT WaifuId FROM UserWaifus WHERE UserId = ? AND WaifuId IN ({})".format(
+        ','.join(['?'] * len(list_of_waifu_ids)))
+    return Zdb.get_json_result(query, args=(user_token, *list_of_waifu_ids))
 
 
 def delete_waifu(user_id, waifu_id):
