fe9d774a065a044333e6782163866f8c61be91cf
ZUNJAE <sunjayskhr@gmail.com>
2018-05-10 02:55:04 +0200

    Update AnimeRepository.py

--- source code/AnimeRepository.py
+++ source code/AnimeRepository.py
@@ -2,4 +2,4 @@ import Zdb
 
 
 def get_relations(mal_id):
-     return Zdb.get_json_result("SELECT DISTINCT * FROM AnimeEntries WHERE Id ?", (mal_id,))
+     return Zdb.get_json_result("SELECT DISTINCT * FROM AnimeEntries WHERE Id = ?", (mal_id,))
