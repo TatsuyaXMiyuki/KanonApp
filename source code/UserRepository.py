935b14c4cce0d1652498119d92295a4dca65a459
ZUNJAE <9930542+zunjae@users.noreply.github.com>
2018-08-23 15:33:24 +0200

    Update UserRepository.py
    forgot this

--- source code/UserRepository.py
+++ source code/UserRepository.py
@@ -2,12 +2,17 @@ import random
 import string
 import time
 
-import Zdb
+from KanonApp import Zdb
 
 
 def create_new_user(google_user_id):
-    pass
+    current_date = int(time.time())
+    user_id = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10))
+    Zdb.insert("INSERT INTO Users(UserId, GoogleUserId, DateCreated) VALUES (?, ?, ?)",
+               (user_id, google_user_id, int(current_date)))
+    return user_id
 
 
 def get_user_token_by_google_id(google_user_id):
-    pass
+    if google_user_id is not None and len(google_user_id) > 0:
+        return Zdb.get_simple_value("SELECT UserId FROM Users WHERE GoogleUserID = ?", (google_user_id,))
